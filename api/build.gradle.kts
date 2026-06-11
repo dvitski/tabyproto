21c973f58a0f63242286dfe5e4742c993f2092f3
Daniel Orr <orrdaniel73@gmail.com>
2026-06-12 00:57:10 +0100

    build: add JmDNS dependency and JUnit5 test infrastructure to api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,6 +10,14 @@ dependencies {
     api("com.squareup.okhttp3:okhttp:4.12.0")
     api("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.27")
+    implementation("org.jmdns:jmdns:3.6.1")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 kotlin {
