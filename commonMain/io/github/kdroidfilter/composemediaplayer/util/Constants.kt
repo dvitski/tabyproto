f5b03aee24578a534f3d0100629184cba25ada99
Daniel Orr <orrdaniel73@gmail.com>
2026-06-12 21:40:58 +0100

    fix: remove commonMain/

--- commonMain/io/github/kdroidfilter/composemediaplayer/util/Constants.kt
+++
@@ -1,3 +0,0 @@
-package io.github.kdroidfilter.composemediaplayer.util
-
-internal const val DEFAULT_ASPECT_RATIO = 16f / 9f
--- commonMain/io/github/kdroidfilter/composemediaplayer/util/Uri.kt
+++
@@ -1,5 +0,0 @@
-package io.github.kdroidfilter.composemediaplayer.util
-
-import io.github.vinceglb.filekit.PlatformFile
-
-expect fun PlatformFile.getUri(): String
