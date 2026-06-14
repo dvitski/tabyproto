cc12269e6123c29a1a7e26b69917ee005f045e10
Daniel Orr <orrdaniel73@gmail.com>
2026-06-14 23:11:31 +0100

    build: restore composemediaplayer version comment

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation(compose.desktop.currentOs)
     implementation(compose.materialIconsExtended)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    implementation("io.github.kdroidfilter:composemediaplayer:0.10.0")
+    implementation("io.github.kdroidfilter:composemediaplayer:0.10.0") // 0.10.1 not yet synced to Maven Central
     implementation("org.bytedeco:javacv:1.5.11")
     implementation("org.bytedeco:ffmpeg:6.1.1-1.5.11:windows-x86_64")
 
