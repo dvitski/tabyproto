12a877d076f9f12966b88965144eaa34c8c85054
Daniel Orr <orrdaniel73@gmail.com>
2026-06-15 12:13:59 +0100

    feat: enforce 520x460dp minimum window size

--- desktop/src/main/kotlin/cc/dvitski/tabyproto/desktop/Main.kt
+++ desktop/src/main/kotlin/cc/dvitski/tabyproto/desktop/Main.kt
@@ -1,10 +1,13 @@
 package cc.dvitski.tabyproto.desktop
 
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 fun main() = application {
     val appState = remember { AppState() }
@@ -16,6 +19,12 @@ fun main() = application {
         title = "TabyProto",
         state = rememberWindowState(width = 1100.dp, height = 860.dp),
     ) {
+        val density = LocalDensity.current
+        SideEffect {
+            window.minimumSize = with(density) {
+                Dimension(520.dp.roundToPx(), 460.dp.roundToPx())
+            }
+        }
         App(appState)
     }
 }
